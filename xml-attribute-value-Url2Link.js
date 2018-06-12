bf20d688362ad1d4ff134dcf1fc1007d5a34094f
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-06-12 15:31:39 +0200

    Update xml-attribute-value-Url2Link.js

--- xml-attribute-value-Url2Link.js
+++ xml-attribute-value-Url2Link.js
@@ -2,10 +2,10 @@
 	const m=b.querySelectorAll('.html-attribute-value');
 	let i=m.length;
 	while(i){
-		const e=m[--i],p=/(\s+)*(https?:\/\/[^\s\(\)\'\"\<\>]+)(\s+)*/ig;
+		const e=m[--i],p=/(?:\s+)*(https?:\/\/[^\s\(\)\'\"\<\>]+)(?:\s+)*/ig;
 		let s=e.innerHTML;
 		if(p.test(s)){
-			e.innerHTML=s.replace(p,'<a target="_blank" href="$2">$2</a>');
+			e.innerHTML=s.replace(p,'<a target="_blank" href="$1">$1</a>');
 		}
 	}
 })(document.body);
