30d81ec90fbf2ede2a370bd96f480123f371d63d
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-05-01 14:11:57 +0200

    Update dump2console.js

--- Chrome_Bookmarks-ExportAnIndividualFolder/dump2console.js
+++ Chrome_Bookmarks-ExportAnIndividualFolder/dump2console.js
@@ -1,11 +1,13 @@
 (w=>{
 	console.clear();
+
 	const dump=m=>{
 		if(0!==m.length){
 			while(0!==m.length){//dumpNode!
 				const n=m.shift(),t=n.title||n.id,c=n.children;
 				if(c){
 					if(0!==c.length){
+						//console.info('%o',n);
 						console.groupCollapsed(t);
 						dump(c);
 						console.groupEnd()
@@ -16,5 +18,6 @@
 			}
 		}
 	};
+
 	w.chrome.bookmarks.getTree(dump);
 })(window);
