3da755b23c9a0a072bd037de42ac0de2ec01fa99
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-12-08 22:52:19 +0100

    Update getStyleSheets.js

--- getStyleSheets.js
+++ getStyleSheets.js
@@ -1,47 +1,53 @@
 //javascript:
 (async w=>{
-	const e=w.$0;
-	if(e){
-		const XHR=w.XMLHttpRequest,
-		P=w.Promise,
-		req=(i,url)=>{
-		  return new P((resolve,reject)=>{
-			const o=new XHR();
-			o.responseType='text';
-			o.onreadystatechange=()=>{
-				if(4===o.readyState){
-					resolve([i,200!==o.status?'':o.response])
-				}
-			};
-			o.onerror=()=>resolve('');
-			o.open('GET',url,true);
-			o.send(null)
-		  })
-		},
-		promises=[],
-		result=[], 
-		slot=[];
-		for(const sheet of w.document.styleSheets){
-			if(true !== sheet.disabled){
-				let x;
-				if(x=sheet.href){
-					if(x.length){
-						console.log(x);
-						const m=result,i=result.length;
-						m[i]=['/*\nbad-url:\t','\n*/'].join(x)
-						promises.push(req(i,x))
-					}
-				}else{
-					if(x=sheet.ownerNode){
-						result.push(x.innerHTML)
-					}
-				}
+	const d=w.document,
+	request=(i,x)=>{
+	  return new Promise(resolve=>{
+		const r=new XMLHttpRequest();
+		r.responseType='text';
+		r.onreadystatechange=()=>{
+			if(4===r.readyState){
+				resolve([i,[x,200!==r.status?c2.join(x):r.response.trim()]])
 			}
 		};
-		P.all(promises).then(a=>{
-			const m=result;
-			a.forEach(v=>{m[v[0]]=v[1]});
-			console.dir(m)
-		})
-	}
+		r.onerror=()=>resolve(c1.join(x));
+		r.open('GET',x,true);
+		r.send(null)
+	  })
+	},
+	c1=['/*\n\u1F6D1:\t','\n*/'],
+	c2=['/*\n\u26A0:\t','\n*/'],
+	c3=['/*\nempty:\t','\n*/'],
+	c4=['/*\n','\n*/'],
+	promises=[],
+	result=[];
+	for(const sheet of d.styleSheets){
+		if(!sheet.disabled){
+			let x;
+			if(x=sheet.href){
+				if(x.length){
+					const m=result,i=result.length;
+					m[i]=null;
+					promises.push(request(i,x))
+				}
+			}else{
+				if(x=sheet.ownerNode){
+					result.push(x.innerHTML)
+				}
+			}
+		}
+	};
+	Promise.all(promises).then(a=>{
+		const m=result,
+		p=(
+			a.forEach(v=>{
+				const a=v[1],t=a[1];
+				m[v[0]]=[(t.length?c4:c3).join(a[0]),t].join('\n\n')
+			}),
+			w.open().document.body.appendChild(d.createElement("P"))
+		);
+		p.style='height:100%;overflow:hidden\u0020scroll;white-space:pre-line;';
+		p.textContent=m.join('\n\n');
+		//console.dir(m)
+	})
 })(window);
